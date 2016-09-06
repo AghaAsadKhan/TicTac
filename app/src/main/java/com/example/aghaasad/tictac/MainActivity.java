b520cbf24a5360455185313b5a791345914cd734
AghaAsadKhan <themragha@gmail.com>
2016-09-06 13:02:32 +0500

    initial commit

---
+++ app/src/main/java/com/example/aghaasad/tictac/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.aghaasad.tictac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    public void dropin(View view)
+    {
+
+        ImageView counter=(ImageView) view;
+        counter.setTranslationY(-1000f);
+        counter.setImageResource(R.drawable.tak);
+        counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+
+
+
+}
