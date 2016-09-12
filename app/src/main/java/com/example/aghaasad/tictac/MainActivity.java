14d21ea08d81831a24a9376a878aed5a10add32e
AghaAsadKhan <themragha@gmail.com>
2016-09-12 18:05:27 +0500

    check game state

--- app/src/main/java/com/example/aghaasad/tictac/MainActivity.java
+++ app/src/main/java/com/example/aghaasad/tictac/MainActivity.java
@@ -6,18 +6,46 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    // 0=yellow; 1=red;
+
+    int activePlayer=0;
+
+    // 2 means unplayed
+    int[] gameState={2,2,2,2,2,2,2,2,2};
 
 
 
-    public void dropin(View view)
-    {
 
-        ImageView counter=(ImageView) view;
-        counter.setTranslationY(-1000f);
-        counter.setImageResource(R.drawable.tak);
-        counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
 
+    public void dropin(View view) {
+
+        ImageView counter = (ImageView) view;
+        //System.out.println(counter.getTag().toString());
+
+        // getting tapped column/row tag....
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+
+        if (gameState[tappedCounter] == 2) {
+
+            gameState[tappedCounter]=activePlayer;
+
+            counter.setTranslationY(-1000f);
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.tik);
+
+                activePlayer = 1;
+
+            } else {
+                counter.setImageResource(R.drawable.tak);
+                activePlayer = 0;
+            }
+
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+        }
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
